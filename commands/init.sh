7f46f6a912ecdf8685ff67cf5f8e6b758404d192
João Carlos Ferra de Almeida <jcfd.almeida@campus.fct.unl.pt>
2021-06-09 10:51:19 +0100

    Update init.sh

--- commands/init.sh
+++ commands/init.sh
@@ -11,7 +11,7 @@ chmod +x vars
 cd /var/lib/waagent/custom-script/download/0/project/bloc-server/
 
 # Starts the network with the args given
-/var/lib/waagent/custom-script/download/0/project/bloc-server/minifab up -o faculty.com -c channel${2} -n testcc -l go -p \"init\",\"${2}\",\"${3}\",\"${1}\"
+/var/lib/waagent/custom-script/download/0/project/bloc-server/minifab up -o faculty.com -c channel${2} -e 7300 -n testcc -l go -p \"init\",\"${2}\",\"${3}\",\"${1}\"
 
 
 
